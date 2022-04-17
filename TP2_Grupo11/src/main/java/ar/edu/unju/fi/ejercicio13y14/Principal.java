802858cc01b0f1cf0f5a6a6dc72d18b08ebc86dd
emanuelAlavar <manusovich15@gmail.com>
2022-04-17 20:17:32 -0300

    Creacion de nueva rama y agregado de puntos 3 4 13 14 y 15

---
+++ TP2_Grupo11/src/main/java/ar/edu/unju/fi/ejercicio13y14/Principal.java
@@ -0,0 +1,29 @@
+package ar.edu.unju.fi.ejercicio13y14;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Empleado empleado = new Empleado();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingrese horas trabajadas:");
+		int horasT= sc.nextInt();
+		sc.close();
+		System.out.println("Sueldo: $"+empleado.calcularSueldo(horasT));
+		empleado.setSueldo(empleado.calcularSueldo(horasT));
+		System.out.println(empleado.toString());
+		
+		Empleado empleadoUno = new Empleado();
+		Empleado empleadoDos = new Empleado("Emanuel Alavar",3333,150);
+		Empleado empleadoTres = new Empleado("Hector Cruz","2022-07-12",190);
+		Empleado empleadoCuatro = new Empleado("Alejandro Anachuri",160);
+		
+		
+		System.out.println(empleadoUno.toString());
+		System.out.println(empleadoDos.toString());
+		System.out.println(empleadoTres.toString());
+		System.out.println(empleadoCuatro.toString());
+	}
+
+}
---
+++ TP2_Grupo11/src/main/java/ar/edu/unju/fi/ejercicio4/Mes.java
@@ -0,0 +1,50 @@
+package ar.edu.unju.fi.ejercicio4;
+
+public class Mes {
+	private int numero;
+
+	public Mes() {
+		
+	}
+	
+	public Mes(int numero) {
+		this.numero = numero;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public String esMes(int numero) {
+		if(numero==1)
+			return "SI es un mes valido";
+		if(numero==2)
+			return "SI es un mes valido";
+		if(numero==3)
+			return "SI es un mes valido";
+		if(numero==4)
+			return "SI es un mes valido";
+		if(numero==5)
+			return "SI es un mes valido";
+		if(numero==6)
+			return "SI es un mes valido";
+		if(numero==7)
+			return "SI es un mes valido";
+		if(numero==8)
+			return "SI es un mes valido";
+		if(numero==9)
+			return "SI es un mes valido";
+		if(numero==10)
+			return "SI es un mes valido";
+		if(numero==11)
+			return "SI es un mes valido";
+		if(numero==12)
+			return "SI es un mes valido";
+		else
+			return "NO EXISTE este mes";
+	}
+}
